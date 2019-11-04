1aeea132a6f45654c5253e0eac0282f01770a584
quangvinh124 <53868788+nguyenquangvinh2000@users.noreply.github.com>
2019-11-04 13:36:38 +0700

    Update bai6.py

--- bai6.py
+++ bai6.py
@@ -1,32 +1,18 @@
-x = int(input("nhap thang: "))
+thang = int(input("nhap thang: "))
+nam = int(input("nhap nam: "))
+if thang == 2:
+	if(nam%4 == 0 and nam/100!=0) or nam%400 == 0:
+		songay = 29
+		
 
-    if x == 1:
-        print("thang nay co 31 ngay")
-    elif x == 2:
-        print("thang nay co 28 ngay")
-        print("nam nay co 364 ngay")
-    elif x == 2*:
-        print("thang nay co 29 ngay")
-        print("nam nay co 365 ngay")
-    elif x == 3:
-        print("thang nay co 31 ngay")
-    elif x == 4:
-        print("thang nay co 30 ngay")
-    elif x == 5:
-        print("thang nay co 31 ngay")
-    elif x == 6:
-        print("thang nay co 30 ngay")
-    elif x == 7:
-        print("thang nay co 31 ngay")
-    elif x == 8:
-        print("thang nay co 31 ngay")
-    elif x == 9:
-        print("thang nay co 30 ngay")
-    elif x == 10;
-        print("thang nay co 31 ngay")
-    elif x == 11:
-        print("thang nay co 30 ngay")
-    elif x == 12:
-        print("thang nay co 31 ngay")
+	else:	
+		songay = 28
 
+
+elif thang in [1,3,5,7,8,10,12]:
+	songay = 31
+elif thang in [4,6,9,11]:
+	songay = 30
+
+print("so ngay :",songay)
 
