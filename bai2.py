f23c96bdc48180a1da0b6c57dc6a04d1e4005464
quangvinh124 <53868788+nguyenquangvinh2000@users.noreply.github.com>
2019-11-04 09:18:31 +0700

    Update bai2.py

--- bai2.py
+++ bai2.py
@@ -2,9 +2,9 @@ print("gia tri cua x")
 x = int(input())
 max = 0
 while x > 0:
-    sodu = int(x % 10)
-    x = int(x/10)
-    if sodu > max:
-        max = sodu
+    y = int(x % 10)
+    y = int(x/10)
+    if y > max:
+        max = y
 
 print("so lon nhat la: ",max)
