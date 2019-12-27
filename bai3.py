2a9da32d453c874cceea070cfdbf7797266ea342
quangvinh124 <53868788+nguyenquangvinh2000@users.noreply.github.com>
2019-12-27 17:57:43 +0700

    Update bai3.py

--- bai3.py
+++ bai3.py
@@ -2,6 +2,9 @@ a=float(input("a="))
 b=float(input("b="))
 x=input("x: ")
 if x == "+" : print(a+b)
-elif x == "-" : print(a-b)
-elif x == "*" : print(a*b)
-elif x == "/" : print(a/b)
+elif x == "-": 
+  print(a-b)
+elif x == "*": 
+  print(a*b)
+elif x == "/": 
+  print(a/b)
